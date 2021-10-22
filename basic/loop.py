78475bfa4c4072ba9c9849eccc326e3ce5d40652
Nse <5udoun19@solent.ac.uk>
2021-10-22 14:49:48 +0100

    Created a program that reads the first line

--- basic/loop.py
+++ basic/loop.py
@@ -2,4 +2,4 @@ print("how many times should i repeat")
 
 numberoftimes = int(input("enter number here: "))
 for number in range(numberoftimes):
-    print("my name is nse")
+    print("This is repeated")
